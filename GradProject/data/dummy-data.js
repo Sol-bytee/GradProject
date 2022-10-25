d9a8e1c84efb419e41aae82b87923ce454b54ec9
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2022-10-26 01:42:52 +0300

    Add files via upload

---
+++ GradProject/data/dummy-data.js
@@ -0,0 +1,10 @@
+import Hall from '../models/Hall';
+
+export const HALLS = [
+  new Hall(1, 'AL SAMA', 20000, 25, 'AAAAAAAAAAAAAAAA', [true, false, true]),
+  new Hall(2, 'AL ELA', 24000, 44, 'BBBBBBBBBBBBBBBBBBBBB', [true, true, true]),
+  new Hall(3, 'AL BROT', 30000, 34, 'CCCCCCCCCCCCCCCCCCCCCC', [true, false, false]),
+  new Hall(4, 'AL REEM', 22000, 15, 'DDDDDDDDDDDDDDDDDDDDDDDDDDDDD', [false, false, true]),
+  new Hall(5, 'AL NEEM', 15000, 22, 'EEEEEEEEEEE', [true, false, true]),
+  new Hall(6, 'AL SOOG', 21000, 27, 'FFFFFFFFFFFFFFFFFFFFFFF', [false, false, true]),
+];
---
+++ GradProject/models/Hall.js
@@ -0,0 +1,12 @@
+class Hall {
+  constructor(id, name, price, guests, description, services) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.guests = guests;
+    this.description = description;
+    this.services = services;
+  }
+}
+
+export default Hall;
