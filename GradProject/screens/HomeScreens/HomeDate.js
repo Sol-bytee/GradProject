e6af2b7389ea7db797c58828eebc8d24a0ff1476
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2022-11-01 02:07:08 +0300

    Add files via upload

---
+++ GradProject/screens/HomeScreens/HomeDate.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+function HomeDate(){
+    return(
+        <View>
+            <Text>Date</Text>
+        </View>
+    );
+}
+
+export default HomeDate;
---
+++ GradProject/screens/HomeScreens/HomeFilter.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+function HomeFilter(){
+    return(
+        <View>
+            <Text>Filter</Text>
+        </View>
+    );
+}
+
+export default HomeFilter;
