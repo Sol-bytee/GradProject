3dbe57b74eedfbfd887a5de67aa121f84937c97d
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2022-11-01 02:11:25 +0300

    Update HallList.js

--- GradProject/components/common/HallList.js
+++ GradProject/components/common/HallList.js
@@ -1,18 +1,28 @@
-import { View, FlatList, StyleSheet } from 'react-native';
+import { useState,useCallback } from "react";
+import { View, FlatList, RefreshControl, StyleSheet } from 'react-native';
 
 import HallView from "./HallView";
 
-function HallList({Halls}){
+const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
+function HallList({Halls, navigation}){
+    const [refreshing, setRefreshing] = useState(false);
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        wait(500).then(() => setRefreshing(false));
+    }, []);
     
     //كل اكله لها عنصر
     function renderHallInfo(itemData){
- 
         const item = itemData.item;
         const hallInfoProps ={
-            id: item.id, //هذا عشان نعرف ايدي الاكله اللي بننتقل لها بعدين
-            title: item.title,
+            id: item.id, //هذا عشان نعرف ايدي قاعة اللي بننتقل لها بعدين
+            name: item.name,
             price: item.price,
-            guests: item.guests
+            guests: item.guests,
+            imageUrl: item.imageUrl
         };
         return (
             <HallView {...hallInfoProps}/>
@@ -20,13 +30,19 @@ function HallList({Halls}){
     }
 
     return( 
-        //الاكل الموجود بكل تصنيف
+        //عرض القاعات
         <View style={styles.view1}>
             <FlatList
                 style={styles.flat1}
                 data={Halls}
                 keyExtractor={(item) => item.id}
                 renderItem={renderHallInfo}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
             />
         </View>
     );
@@ -36,6 +52,9 @@ export default HallList;
 
 const styles = StyleSheet.create({
     view1:{
+        flex: 1,
+    },
+    flat1:{
         flex: 1,
     },
   });
