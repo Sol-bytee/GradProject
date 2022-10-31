75237e811e561275b83c51da90e5aa358f8da1d5
Sol-bytee <113649033+Sol-bytee@users.noreply.github.com>
2022-11-01 02:07:49 +0300

    Add files via upload

---
+++ GradProject/models/Hall.js
@@ -0,0 +1,13 @@
+class Hall {
+  constructor(id, name, price, guests, imageUrl, description, services) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.guests = guests;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.services = services;
+  }
+}
+
+export default Hall;
